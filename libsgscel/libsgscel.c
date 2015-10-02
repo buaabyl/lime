3c3b5b140fbdca6c464a71f38dbfbd154248c37c
buaa.byl <buaa.byl@gmail.com>
2015-10-02 22:13:28 +0800

    rename

--- libsgscel/libsgscel.c
+++ libsgscel/libsgscel.c
@@ -12,7 +12,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "sougou_dict.h"
+#include "libsgscel.h"
 
 ///////////////////////////////////////////////////////////////////////////
 //Helpher interface declare
